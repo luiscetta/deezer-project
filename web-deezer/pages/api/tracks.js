2b52e3237c289a998dffac5b2702836545ded8ee
luiscetta <filipe.cetta@gmail.com>
2021-12-31 10:34:53 -0300

    refactoring api code

--- web-deezer/pages/api/tracks.js
+++ web-deezer/pages/api/tracks.js
@@ -1,13 +1,14 @@
 import axios from "axios";
 
-const url = "https://api.deezer.com/artist/27/top";
+const url = "https://api.deezer.com/chart";
 
-function getTracks() {
-    axios.get(url)
-    .then(response => {
-        console.log(response)
-    })
-    .catch(error => console.log(error))
+export default async function getTracks() {
+    try {
+        const tracks = (await axios.get(url)).data;
+        return tracks;
+    } catch (err) {
+        console.error(err);
+    }
 }
 
 getTracks();
